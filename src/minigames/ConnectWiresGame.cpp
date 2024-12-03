225c1a2fb44566b1fcad9af459454c3ae9e67451
prbartosh <pajorb4@gmail.com>
2024-12-03 20:12:29 +0100

    small changes to wires minigame

--- src/minigames/ConnectWiresGame.cpp
+++ src/minigames/ConnectWiresGame.cpp
@@ -2,22 +2,24 @@
 
 ConnectWiresGame::ConnectWiresGame(float x, float y, float width, float height, const std::string& title)
     : MiniGame(x, y, width, height, title), mouse(GetMousePosition()), moveStartPoint(false), moveEndPoint(false) {
+    // wirePoints.at(0).at(0) = {window.x + 30, window.y + 60};
+    // wirePoints.at(0).at(1) = {window.x + window.width - 30, window.y + window.height - 30};
     startPoint = {window.x + 30, window.y + 60};
-    endPoint = {window.x + window.width - 30, window.y + window.height - 30};
+    endPoint = {window.x + window.width - 30, window.y + window.height - 35};
     startArea = {window.x + 30, window.y + 40, 35, window.height - 60};
     endArea = {window.x + window.width - 65, window.y + 40, 35, window.height - 60};
-}   
+    
+    createWirePoints(wirePoints);
+}
 
-void MoveToArea(Vector2& point, Rectangle& area);
-void DragAreas(Rectangle& startArea, Rectangle& endArea, bool& isDragable);
 
 void ConnectWiresGame::Update() {
     MiniGame::Update(); // Handle dragging
     Vector2 mouse = GetMousePosition();
 
-    DragAreas(startArea, endArea, isDragable);
-    MoveToArea(startPoint, startArea);
-    MoveToArea(endPoint, endArea);
+    DragObjects(startArea, endArea, wirePoints[0], wirePoints[1], isDragable);
+    MoveToArea(wirePoints[0], startArea);
+    MoveToArea(wirePoints[1], endArea);
 
     // moving balls
     if (CheckCollisionPointCircle(mouse, startPoint, 10.0f) /*&& CheckCollisionPointRec(startPoint, startArea)*/ && (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || (isDragable && !CheckCollisionPointCircle(mouse, startPoint, 10.0f)))) moveStartPoint = true;
@@ -36,40 +38,70 @@ void ConnectWiresGame::Update() {
 
 
 void ConnectWiresGame::Draw() {
-    MiniGame::Draw(); // Draw window
-
-
-    DrawLineBezier(startPoint, endPoint, 4.0f, (Color) {255, 161, 0, 255});
-
+    MiniGame::Draw(); // Draw window 
     //test rectangle for starting points
     DrawRectangleLinesEx({window.x + 30, window.y + 40, 35, window.height - 60}, 2, WHITE);
-
     //test rectangle for ending points
     DrawRectangleLinesEx({window.x + window.width - 65, window.y + 40, 35, window.height - 60}, 2, WHITE);
 
+
+    // connecting line
+    DrawLineBezier(wirePoints[0], wirePoints[1], 4.0f, (Color) {255, 161, 0, 255});
+
     // Draw start-end spline circles with some details
-    DrawCircleV(startPoint, CheckCollisionPointCircle(mouse, startPoint, 10.0f)? 14.0f : 8.0f, moveStartPoint? ORANGE : WHITE);
-    MoveToArea(startPoint, startArea); // move startPoint to startArea
-    DrawCircleV(endPoint, CheckCollisionPointCircle(mouse, endPoint, 10.0f)? 14.0f : 8.0f, moveEndPoint? ORANGE : WHITE);
-    MoveToArea(endPoint, endArea); // move endPoint to endArea
+    DrawCircleV(wirePoints[0], CheckCollisionPointCircle(mouse, wirePoints[0], 10.0f)? 14.0f : 8.0f, moveStartPoint? ORANGE : WHITE);
+    //MoveToArea(wirePoints[0], startArea); // move startPoint to startArea
+    DrawCircleV(wirePoints[1], CheckCollisionPointCircle(mouse, wirePoints[1], 10.0f)? 14.0f : 8.0f, moveEndPoint? ORANGE : WHITE);
+    //MoveToArea(wirePoints[1], endArea); // move endPoint to endArea
 }
 
-void MoveToArea(Vector2& point, Rectangle& area) {
-    if ((point.x) >= (area.x + area.width)) point.x = area.x + (area.width/2);
-    else if (point.x <= (area.x)) point.x = area.x + (area.width/2);
-    else if (point.y >= area.y + area.height) point.y = area.y + area.height - 1;
-    else if (point.y <= area.y) point.y = area.y + 1;
+
+void ConnectWiresGame::createWirePoints(std::vector<Vector2>& points) {
+    int points_num = 16;
+    points.reserve(points_num);
+    for (int i = 0; i < points_num; i++) {
+        if (i % 2 == 0)
+            points[i] = {startArea.x + (startArea.width/2), startArea.y + ((i / 2) + 1)*(startArea.height/8)/2};
+        else if (i % 2 == 1) {
+            points[i] = {endArea.x + (endArea.width/2), endArea.y + endArea.height - ((i + 1)/2)*(endArea.height/8)/2};
+            TraceLog(LOG_INFO, "succesfully created pair %i", ((i + 1)/2));}
+    }
 }
 
-void DragAreas(Rectangle& startArea, Rectangle& endArea, bool& isDragable){
+
+
+void ConnectWiresGame::MoveToArea(Vector2& point, Rectangle& area)  {
+    if ((point.x) >= (area.x + area.width)) 
+        point.x = area.x + (area.width/2);
+    else if (point.x <= (area.x))
+        point.x = area.x + (area.width/2);
+    else if (point.y >= area.y + area.height) 
+        point.y = area.y + area.height - 1;
+    else if (point.y <= area.y) 
+        point.y = area.y + 1;
+}
+
+
+void ConnectWiresGame::DragObjects(Rectangle& startArea, Rectangle& endArea, Vector2& startpoint, Vector2& endpoint, bool& isDragable){
     if (isDragable) { // if window is beeing dragged
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             // update areas coordinates for start/end points
             Vector2 mouseDelta = GetMouseDelta();
             startArea.x += mouseDelta.x;
+            startpoint.x += mouseDelta.x;
             startArea.y += mouseDelta.y;
+            startpoint.y += mouseDelta.y;
             endArea.x += mouseDelta.x;
+            endpoint.x += mouseDelta.x;
             endArea.y += mouseDelta.y;
+            endpoint.y += mouseDelta.y;
+
         }
     }
 }
+
+
+
+
+
+
--- src/minigames/ConnectWiresGame.h
+++ src/minigames/ConnectWiresGame.h
@@ -2,21 +2,28 @@
 #define CONNECT_WIRES_GAME_H
 
 #include "../headers/MiniGameManager.h"
+#include <vector>
+#include <memory>
 
 class ConnectWiresGame : public MiniGame
 {
 private:
-    Vector2 mouse;
     bool moveStartPoint;
     bool moveEndPoint;
+    Vector2 mouse;
     Vector2 startPoint;
     Vector2 endPoint;
     Rectangle startArea;
     Rectangle endArea;
-    
+    std::vector<Vector2> wirePoints;
+    void createWirePoints(std::vector<Vector2>& points);
+    void MoveToArea(Vector2& point, Rectangle& area);
+    void DragObjects(Rectangle& startArea, Rectangle& endArea, Vector2& startpoint, Vector2& endpoint, bool& isDragable);
+
 public:
     // constructor
     ConnectWiresGame(float x, float y, float width, float height, const std::string& title);
+    // standard destructor
     ~ConnectWiresGame() = default;
 
     void Update() override;
