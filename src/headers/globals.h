ad098434f2f7401e398a77188f014a1370a9e51f
Ridicc <franek.razny@gmail.com>
2025-01-03 22:18:56 +0100

    rewritten cmakelists.txt

--- src/headers/globals.h
+++ src/headers/globals.h
@@ -12,11 +12,22 @@
 #include "../managers/GameplayManager.h"
 
 #if defined(_WIN32)
-#include <windows.h>
+    // #include <windows.h> // Removed to prevent conflicts
+    #include "raylib.h"
+    
+    // Define necessary Windows types manually
+    typedef void* HMODULE;
+    typedef char* LPSTR;
+    typedef unsigned long DWORD; // Added definition for DWORD
+    #define WINAPI __stdcall     // Added definition for WINAPI
+    
+    // Declare only GetModuleFileNameA
+    extern "C" __declspec(dllimport) DWORD WINAPI GetModuleFileNameA(HMODULE hModule, LPSTR lpFilename, DWORD nSize);
 #elif defined(__APPLE__)
-#include <mach-o/dyld.h>
+    #include <mach-o/dyld.h>
 #else
-#include <dlfcn.h>
+    #include <dlfcn.h>
+    #include <unistd.h>
 #endif
 
 typedef enum GameScreen { LOGO = 0, TITLE, SETTINGS, GAMEPLAY, ENDING } GameScreen;
