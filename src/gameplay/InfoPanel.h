4102635e7873693266ff13e4d3ba0eaf7ca46e95
Ridicc <franek.razny@gmail.com>
2025-01-07 09:23:50 +0100

    Revert "Merge branch 'pajor'"
    This reverts commit f4760024b6521bf31328b17547d95c0fd80acd66, reversing
    changes made to b9239308b9ae7fc277ee6986e23293a1838e1c9a.

--- src/gameplay/InfoPanel.h
+++ src/gameplay/InfoPanel.h
@@ -2,15 +2,12 @@
 #define INFOPANEL_H
 
 #include "raylib.h"
-#include <vector>
-#include <string>
 
 class InfoPanel {
 public:
     InfoPanel();
     void Render();
 private:
-    void DrawIconDescription(std::vector<std::string> iconDescription, Vector2 iconDescriptionStartingPoint);
 
 };
 
